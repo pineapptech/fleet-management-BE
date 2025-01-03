cdce99d6ba559bb39dd8e331533b72ca2dbced4c
DGreegman <obeagug@gmail.com>
2025-01-03 05:51:30 +0100

    Maintenance Routes updated

--- src/routes/maintenance.route.ts
+++ src/routes/maintenance.route.ts
@@ -4,6 +4,11 @@ import maintenanceController from '../controllers/maintenance.controller';
 
 const maintenanceRoute = Router();
 
-maintenanceRoute.route('/maintenance-record').post(multerMiddleware.upload.single('invoice_img_url'), maintenanceController.createMaintenance);
-maintenanceRoute.route('/maintenance-record').get(maintenanceController.getAllMaintenedVehicles);
+maintenanceRoute.route('/maintenance-record').post(multerMiddleware.upload.single('invoice_img_url'), maintenanceController.createMaintenance).get(maintenanceController.getAllMaintenedVehicles);
+maintenanceRoute
+    .route('/maintenance-record/:id')
+    .get(maintenanceController.getVehicleMaintenanceById)
+    .patch(maintenanceController.updateVehicleMaintenance)
+    .delete(maintenanceController.deleteVehicleMaintenance);
+
 export default maintenanceRoute;
